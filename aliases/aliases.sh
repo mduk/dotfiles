baf94587012e598ec0e904884e3c8bfa25b641ae
Daniel Kendell <daniel@kendell.org>
2015-02-21 16:09:28 +0000

    add the vagrant aliases to be loaded. tbh, i can't really remember what the above bit does, i think something about getting the paths right

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -1,9 +1,12 @@
+my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+alias ebp="vim ~/.bash_profile && source ~/.bash_profile"
 alias fucking="sudo"
 alias c="clear"
 alias t="tree"
 alias ll="ls -lh"
 
-source git.sh
-source erlang.sh
-source ruby.sh
+source "$my_dir/git.sh"
+source "$my_dir/erlang.sh"
+source "$my_dir/ruby.sh"
+source "$my_dir/vagrant.sh"
