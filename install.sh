7655983829c027a74a53f4db761c7775f0ee59ef
Daniel Kendell <daniel@kendell.org>
2015-02-24 22:02:04 +0000

    clone using ssh so i can push stuff back

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd $HOME
-git clone https://github.com/mduk/dotfiles.git .dotfiles
+git clone git@github.com:mduk/dotfiles.git .dotfiles
 echo "source \"$HOME/.dotfiles/sourceme.sh\"" >> ~/.bash_profile
 
