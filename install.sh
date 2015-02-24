5430bf1acc4c3fb528534a5cfade1155e3ddf974
Daniel Kendell <daniel@kendell.org>
2015-02-24 22:08:21 +0000

    nice output on install script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 cd $HOME
-git clone git@github.com:mduk/dotfiles.git .dotfiles
-echo "source \"$HOME/.dotfiles/sourceme.sh\"" >> ~/.bash_profile
 
+echo "Cloning mduk/dotfiles into $HOME/.dotfiles"
+git clone git@github.com:mduk/dotfiles.git .dotfiles
+
+echo "Adding source line to $HOME/.bash_profile - Don't forget to source it after installing!"
+echo "" >> $HOME/.bash_profile
+echo "# mduk/dotfiles" >> $HOME/.bash_profile
+echo "source \"$HOME/.dotfiles/sourceme.sh\"" >> $HOME/.bash_profile
+
+echo "Installation complete."
+echo "Now exec: source \"$HOME/.dotfiles/sourceme.sh\""
