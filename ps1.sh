0ca9020c3d0e93492ccec9ea1e6dbd47c36a0b85
Daniel Kendell <daniel.kendell@mendeley.com>
2015-02-25 11:52:49 +0000

    add timestamps to prompts

--- ps1.sh
+++ ps1.sh
@@ -2,4 +2,6 @@ function parse_git_branch_and_add_brackets {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
 }
 
-export PS1="\033[1m[\u@\h:\w]\[\033[0;32m\]\$(parse_git_branch_and_add_brackets) \[\033[0m\]\n\$ "
+TIME="\033[1m[\t]\033[0m"
+
+export PS1="\n\n\033[1m[\u@\h:\w]\[\033[0;32m\]\$(parse_git_branch_and_add_brackets) \[\033[0m\]\n$TIME \$ "
