f4a036fd9033d4e2e84535281a501864dd857677
Daniel Kendell <daniel.kendell@mendeley.com>
2015-02-25 13:57:23 +0000

    causing problems - not sure why

--- ps1.sh
+++ ps1.sh
@@ -4,4 +4,4 @@ function parse_git_branch_and_add_brackets {
 
 TIME="\033[1m[\t]\033[0m"
 
-export PS1="\n\n\033[1m[\u@\h:\w]\[\033[0;32m\]\$(parse_git_branch_and_add_brackets) \[\033[0m\]\n$TIME \$ "
+export PS1="\n\n\033[1m[\u@\h:\w]\[\033[0;32m\]\$(parse_git_branch_and_add_brackets)\[\033[0m\]\$ "
