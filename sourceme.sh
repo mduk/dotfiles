5a790b0eb1bfb284a978be0e013dc7de57d509b6
Daniel Kendell <daniel.kendell@mendeley.com>
2015-03-24 12:16:23 +0000

    added resource alias for re-sourcing dotfiles

--- sourceme.sh
+++ sourceme.sh
@@ -1,6 +1,7 @@
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-alias edf="vim $my_dir && git --work-tree=$my_dir add . && git --work-tree=$my_dir commit $my_dir && source $my_dir/sourceme.sh"
+alias resource="source $my_dir/sourceme.sh"
+alias edf="vim $my_dir && git --work-tree=$my_dir add . && git --work-tree=$my_dir commit $my_dir && resource"
 alias c="clear"
 
 source "$my_dir/ps1.sh"
