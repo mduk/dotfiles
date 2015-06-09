62d88437ad948f8e2415f1eac7b227b3fc1e24ef
Daniel Kendell <daniel@kendell.org>
2015-06-09 20:13:27 +0100

    unbroke prompt. need to come back to this

--- ps1.sh
+++ ps1.sh
@@ -1,11 +1,5 @@
-GIT_BRANCH=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\\[\1\]/'`
-GIT="\[\033[92m\]$GIT_BRANCH\[\033[0m\]"
+function parse_git_branch_and_add_brackets {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
+}
 
-USER_HOST_PATH="\[\033[1m\][\u@\h:\w]\[\033[0m\]"
-
-RUBY_VERSION=`ruby -v | cut -d' ' -f2`
-RUBY="\[\033[35m\][$RUBY_VERSION]\[\033[0m\]"
-
-TIME="\[\033[1m\][\t]\[\033[0m\]"
-
-export PS1="\n\n$USER_HOST_PATH $RUBY $GIT\n$TIME \$ "
+export PS1="\033[1m[\u@\h:\w]\[\033[0;32m\]\$(parse_git_branch_and_add_brackets) \[\033[0m\]\n\$ "
