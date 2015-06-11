b95d2a79bf40697422e95ef914ba48bcf4195703
Daniel Kendell <daniel@kendell.org>
2015-06-11 20:42:01 +0100

    changed resource to dot

--- sourceme.sh
+++ sourceme.sh
@@ -1,6 +1,6 @@
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-alias resource="source $my_dir/sourceme.sh"
+alias dot="source $my_dir/sourceme.sh"
 alias e="vim -p"
 alias edf="vim $my_dir && git --work-tree=$my_dir add . && git --work-tree=$my_dir commit $my_dir && resource"
 alias c="clear"
