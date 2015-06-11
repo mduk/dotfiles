0c29d7673b708cff1738dda89da6e017099bc184
Daniel Kendell <daniel.kendell@mendeley.com>
2015-06-11 11:12:21 +0100

    show ruby version in prompt

--- ps1.sh
+++ ps1.sh
@@ -1,5 +1,9 @@
-function parse_git_branch_and_add_brackets {
+function git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
 }
 
-export PS1="\033[1m[\u@\h:\w]\[\033[0;32m\]\$(parse_git_branch_and_add_brackets) \[\033[0m\]\n\$ "
+function ruby_version {
+  ruby -v | awk '{print $2}' | sed -e 's/\(.*\)/[\1]/'
+}
+
+export PS1="\033[1m[\u@\h:\w]\[\033[0;32m\]\$(git_branch) \033[0;91m$(ruby_version) \[\033[0m\]\n\$ "
