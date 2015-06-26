00f55064f186714c3e79595732f62ec9cccfd1e7
Daniel Kendell <daniel.kendell@mendeley.com>
2015-06-26 12:17:31 +0100

    try both ./vendor/bin/phpunit and ./vendor/composer/bin/phpunit

--- aliases/php.sh
+++ aliases/php.sh
@@ -1,3 +1,12 @@
-alias pu="./vendor/bin/phpunit"
 alias cu="composer update"
 alias ecj="e composer.json"
+
+function pu {
+	if [ -e "vendor/bin/phpunit" ]; then
+		./vendor/bin/phpunit
+	fi
+
+	if [ -e "vendor/composer/bin/phpunit" ]; then
+		./vendor/composer/bin/phpunit
+	fi
+}
