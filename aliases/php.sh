27ffc7baa0fde6210052c8fbf2b56553dd56c846
Daniel Kendell <daniel@kendell.org>
2015-07-19 02:53:49 +0100

    added pe

--- aliases/php.sh
+++ aliases/php.sh
@@ -1,6 +1,27 @@
 alias cu="composer update"
 alias ecj="e composer.json"
 
+function pe {
+  files=()
+  for arg in "$@"; do
+    dir=$( echo "${arg}" | awk -F/ '{print $1}' )
+
+    if [ "${dir}" = "src" ]; then
+      testFile=$( echo "${arg}" | sed -e 's/src/test/' -e 's/.php/Test.php/' )
+      srcFile=${arg}
+    fi
+
+    if [ "${dir}" = "test" ]; then
+      srcFile=$( echo "${arg}" | sed 's/test/src/' )
+      testFile=${arg}
+    fi
+
+    files+=( ${srcFile} ${testFile} )
+  done
+
+  e ${files[*]}
+}
+
 function pu {
 	if [ -e "vendor/bin/phpunit" ]; then
 		./vendor/bin/phpunit
