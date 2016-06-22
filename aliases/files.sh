0101c1e94fce40c649c40038ed70d241d83468a9
Daniel Kendell <daniel@suade.org>
2016-06-23 00:41:22 +0100

    added x to toggle the executable bit

--- aliases/files.sh
+++ aliases/files.sh
@@ -2,12 +2,23 @@ alias t="tree -L 4"
 
 alias h="cd ~"
 alias d="cd ~/dev"
+
 alias up="cd .."
 alias up2="cd ../.."
 
 alias ll="ls -lh"
 
+# Make and Change Into directory
 function mkcd {
-  mkdir $1
+  mkdir -p $1
   cd $1
+}
+
+# Toggle Executable bit on file
+function x {
+    if [[ -x "$1" ]]; then
+        chmod -x "$1"
+    else
+        chmod +x "$1"
+    fi
 }
