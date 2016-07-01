8e468844cd4cef14a57db4edbcec04a9cdf147f2
Daniel Kendell <daniel@suade.org>
2016-07-01 11:03:58 +0100

    no trailing slashes

--- sourceme.sh
+++ sourceme.sh
@@ -1,10 +1,10 @@
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Add .local/bin/ to PATH
-export PATH="$PATH:$HOME/.local/bin/"
+export PATH="$PATH:$HOME/.local/bin"
 
 # Add the Composer bin/ to PATH
-export PATH="$PATH:$HOME/.composer/vendor/bin/"
+export PATH="$PATH:$HOME/.composer/vendor/bin"
 
 # Add my personal bin/ to the front of the PATH, overriding everything else
 export PATH="$HOME/bin/:$PATH"
