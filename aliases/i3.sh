5dd848b4b68903229f2d3844e0f41d8b8157df65
Daniel Kendell <daniel@suade.org>
2016-07-01 11:03:29 +0100

    aliases for saving and restoring i3 workspace layouts

--- aliases/i3.sh
+++ aliases/i3.sh
@@ -1,1 +1,9 @@
 alias ei3="e ~/.config/i3/config"
+
+i3-save() {
+    i3-save-tree --workspace $1 "$HOME/.config/i3/workspace-$1.json"
+}
+
+i3-arrange() {
+    i3-msg "workspace $1; append_layout $HOME/.config/i3/workspace-$1.json"
+}
