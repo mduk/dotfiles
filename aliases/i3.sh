bd5093f0259277c305b210c75e5ff63b1b0e6cc4
Daniel Kendell <daniel@suade.org>
2016-07-02 11:14:43 +0100

    i forgot that i3 leaves comments in the the json files, deal with those...

--- aliases/i3.sh
+++ aliases/i3.sh
@@ -1,7 +1,13 @@
 alias ei3="e ~/.config/i3/config"
 
 i3-save() {
-    i3-save-tree --workspace $1 "$HOME/.config/i3/workspace-$1.json"
+    i3-save-tree --workspace $1 \
+        | sed -e '/instance/d' \
+              -e '/title/d' \
+              -e '/transient_for/d' \
+              -e '/window_role/d' \
+              -e 's|//\(.*\),|\1|' \
+              -e '/\/\//d' > "$HOME/.config/i3/workspace-$1.json"
 }
 
 i3-arrange() {
