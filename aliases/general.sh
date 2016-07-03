beb7835ae0b0c9809e113d0373e56fa27e23ff3c
Daniel Kendell <daniel@suade.org>
2016-07-03 12:06:03 +0100

    added an alias for editing the prompt dotfile

--- aliases/general.sh
+++ aliases/general.sh
@@ -7,6 +7,8 @@ alias evrc="e ~/.vimrc"
 alias g="grep --color=always --exclude=*.swp --exclude .git/ -R -n"
 alias rmb="find . -name *.bkp -exec rm -r {} \;"
 
+alias ep="e ${DEV_DIR}/mduk/dotfiles/ps1.sh"
+
 function dotup {
   pushd ~/.dotfiles
   gf && gp
