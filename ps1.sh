8eb4f66ce32a51fa52a4f56fa59dbbd6b082bacc
Daniel Kendell <daniel@suade.org>
2016-07-03 12:06:33 +0100

    use the PROMPT_COMMAND callback

--- ps1.sh
+++ ps1.sh
@@ -1,43 +1,68 @@
-# Previous command status bar
-PS1="\033[48;5;\$([[ \$? -gt 0 ]] && echo \"88\" || echo \"22\")m\033[K\033[0m"
+terminal_width() {
+    local cols=$(tput cols)
 
-# Distance from previous command
-PS1="${PS1}\n\n"
+    if [[ $cols -eq 210 ]]; then
+        echo "1"
+    elif [[ $cols -lt 210 ]] && [[ $cols -ge 103 ]]; then
+        echo "2"
+    elif [[ $cols -lt 103 ]] && [[ $cols -ge 68 ]]; then
+        echo "3"
+    else
+        echo "4"
+    fi
+}
 
-# User/Host/Directory
-PS1="${PS1}\[\033[1m\][\u@\h:\w]\033[0m\]"
+prompt_command() {
 
-# Break prompt in narrow terminals if we're anywhere other than the home directory
-PS1="${PS1}\$([[ \$(tput cols) -le 80 ]] && [[ \$(pwd) != \$HOME ]] && echo \"\[\n\]\")"
+    PS1=""
 
-# Git Branch
-PS1="${PS1}\[\033[0;32m\]\$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/')\[\033[0m\]"
+    if [[ $(tput lines) -ge 20 ]]; then
 
-# Begin Ruby Block
-PS1="${PS1}\[\033[0;91m\]["
+        # Previous command status bar
+        PS1="\033[48;5;\$([[ \$? -gt 0 ]] && echo \"88\" || echo \"22\")m\033[K\033[0m"
 
-    # Ruby Version
-    PS1="${PS1}$(ruby -v | awk '{print $2}')"
+        # Distance from previous command
+        PS1="${PS1}\n\n"
 
-# End Ruby Block
-PS1="${PS1}]\[\033[0m\]"
+    fi
 
-# Begin Python Block
-PS1="${PS1}\[\033[0;93m\]["
+    # User/Host/Directory
+    PS1="${PS1}\[\033[1m\][\u@\h:\w]\033[0m\]"
 
-    # Python Version
-    PS1="${PS1}$(python --version 2>&1 | awk '{print $2}')"
+    # Break prompt in narrow terminals if we're anywhere other than the home directory
+    PS1="${PS1}\$([[ \$(tput cols) -le 80 ]] && [[ \$(pwd) != \$HOME ]] && echo \"\[\n\]\")"
 
-    # Python VirtualEnv
-    PS1="${PS1}\$([[ -v VIRTUAL_ENV ]] && echo \" venv\")"
+    # Git Branch
+    PS1="${PS1}\[\033[0;32m\]\$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/')\[\033[0m\]"
 
-# End Python Block
-PS1="${PS1}]\[\033[0m\]"
+    # Begin Ruby Block
+    PS1="${PS1}\[\033[0;91m\]["
 
-# Newline
-PS1="${PS1}\n"
+        # Ruby Version
+        PS1="${PS1}$(ruby -v | awk '{print $2}')"
 
-# Timestamp
-PS1="${PS1}\[\033[1m\][\$(date +%k:%M:%S)]\[\033[0m\] \$ "
+    # End Ruby Block
+    PS1="${PS1}]\[\033[0m\]"
+
+    # Begin Python Block
+    PS1="${PS1}\[\033[0;93m\]["
+
+        # Python Version
+        PS1="${PS1}$(python --version 2>&1 | awk '{print $2}')"
+
+        # Python VirtualEnv
+        PS1="${PS1}\$([[ -v VIRTUAL_ENV ]] && echo \" venv\")"
+
+    # End Python Block
+    PS1="${PS1}]\[\033[0m\]"
+
+    # Newline
+    PS1="${PS1}\n"
+
+    # Timestamp
+    PS1="${PS1}\[\033[1m\][\$(date +%k:%M:%S)]\[\033[0m\] \$ "
 
 export PS1="${PS1}"
+}
+
+PROMPT_COMMAND=prompt_command
