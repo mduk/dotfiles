d95d4789d16902d82482e3eee7f0145279f8f2ad
Daniel Kendell <daniel@suade.org>
2016-07-07 22:12:11 +0100

    added venv to create and toggle python virtual environments

--- aliases/python.sh
+++ aliases/python.sh
@@ -8,3 +8,18 @@ function touchmod {
     mkdir $1
     touch $1/__init__.py
 }
+
+# VirtualEnv
+#   If a venv directory doesn't exist, initialise one
+#   Toggle the venv
+function venv {
+    if [[ ! -d venv ]]; then
+        virtualenv venv
+    fi
+
+    if [[ ${VIRTUAL_ENV:=unset} = "unset" ]]; then
+        source venv/bin/activate
+    else
+        deactivate
+    fi
+}
