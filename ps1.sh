3b47539884588cd69985dab6f5ff13c884aa203a
Daniel Kendell <daniel@suade.org>
2016-07-11 13:25:56 +0100

    Added a super-minimal one-line prompt if the window is only two or one lines

--- ps1.sh
+++ ps1.sh
@@ -29,45 +29,49 @@ prompt_command() {
     # User/Host/Directory
     PS1="${PS1}\[\033[1m\][\u@\h:\w]\033[0m\]"
 
-    # Break prompt in narrow terminals if we're anywhere other than the home directory
-    PS1="${PS1}\$([[ \$(tput cols) -le 80 ]] && [[ \$(pwd) != \$HOME ]] && echo \"\[\n\]\")"
+    if [[ $(tput lines) -gt 2 ]]; then
 
-    # Git Branch
-    PS1="${PS1}\[\033[0;32m\]\$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/')\[\033[0m\]"
+        # Break prompt in narrow terminals if we're anywhere other than the home directory
+        PS1="${PS1}\$([[ \$(tput cols) -le 80 ]] && [[ \$(pwd) != \$HOME ]] && echo \"\[\n\]\")"
 
-    # Begin Ruby Block
-    PS1="${PS1}\[\033[0;91m\]["
+        # Git Branch
+        PS1="${PS1}\[\033[0;32m\]\$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/')\[\033[0m\]"
 
-        # Ruby Version
-        PS1="${PS1}$(ruby -v | awk '{print $2}')"
+        # Begin Ruby Block
+        PS1="${PS1}\[\033[0;91m\]["
 
-    # End Ruby Block
-    PS1="${PS1}]\[\033[0m\]"
+            # Ruby Version
+            PS1="${PS1}$(ruby -v | awk '{print $2}')"
 
-    # Begin Python Block
-    PS1="${PS1}\[\033[0;93m\]["
+        # End Ruby Block
+        PS1="${PS1}]\[\033[0m\]"
 
-        # Python Version
-        PS1="${PS1}$(python --version 2>&1 | awk '{print $2}')"
+        # Begin Python Block
+        PS1="${PS1}\[\033[0;93m\]["
 
-        # Python VirtualEnv
-        PS1="${PS1}\$([[ -v VIRTUAL_ENV ]] && echo \" venv\")"
+            # Python Version
+            PS1="${PS1}$(python --version 2>&1 | awk '{print $2}')"
 
-    # End Python Block
-    PS1="${PS1}]\[\033[0m\]"
+            # Python VirtualEnv
+            PS1="${PS1}\$([[ -v VIRTUAL_ENV ]] && echo \" venv\")"
 
-    # Newline
-    PS1="${PS1}\n"
+        # End Python Block
+        PS1="${PS1}]\[\033[0m\]"
 
+        # Newline
+        PS1="${PS1}\n"
+
+    fi
+
+    # Insert Custom Directory Prompt
     if [[ -f "$(pwd)/.ps1" ]]; then
-
         PS1="${PS1}$(source "$(pwd)/.ps1")\n"
     fi
 
     # Timestamp
     PS1="${PS1}\[\033[1m\][\$(date +%k:%M:%S)]\[\033[0m\] \$ "
 
-export PS1="${PS1}"
+    export PS1="${PS1}"
 }
 
 PROMPT_COMMAND=prompt_command
