592baf1acff60312a7fd831c8f43268ae130e6b9
Daniel Kendell <daniel.kendell@gmail.com>
2017-01-14 22:18:46 +0000

    better downloading of youtube playlists

--- aliases/youtube.sh
+++ aliases/youtube.sh
@@ -2,15 +2,43 @@ alias ytdll="youtube-dl -F"
 
 function ytdla() {
     pushd ~/Music
-    youtube-dl -f m4a --output "%(uploader)s/%(title)s.%(ext)s" "${1}"
+    youtube-dl \
+        -f m4a \
+        --output "%(uploader)s/%(title)s.%(ext)s" \
+        "${1}"
     popd
 }
 
-function ytdlv() {
-    pushd ~/Videos
-    youtube-dl -f best --output "%(uploader)s/%(title)s.%(ext)s" "${1}"
+function ytdlapl() {
+    pushd ~/Music
+    youtube-dl \
+        -f m4a \
+        --output "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+        "${1}"
     popd
 }
+
+
+
+function ytdlv() {
+    pushd ~/Videos
+    youtube-dl \
+        -f best \
+        --output "%(uploader)s/%(release_date)s - %(title)s.%(ext)s" \
+        "${1}"
+    popd
+}
+
+function ytdlvpl() {
+    pushd ~/Videos
+    youtube-dl \
+        -f best \
+        --output "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+        "${1}"
+    popd
+}
+
+
 
 function ytdlj() {
     youtube-dl -j $1 | jq .
