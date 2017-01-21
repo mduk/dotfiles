2e29634b7b0f89f2006160081999724d584ab3c3
Daniel Kendell <daniel.kendell@gmail.com>
2017-01-21 23:24:20 +0000

    updated global composer bin path

--- path.sh
+++ path.sh
@@ -2,7 +2,7 @@
 export PATH="$PATH:$HOME/.local/bin"
 
 # Add the Composer bin/ to PATH
-export PATH="$PATH:$HOME/.composer/vendor/bin"
+export PATH="$PATH:$HOME/.config/composer/vendor/bin"
 
 # Add my personal bin/ to the front of the PATH, overriding everything else
 export PATH="$HOME/bin/:$PATH"
