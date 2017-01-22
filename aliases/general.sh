89847e8a2a656e2e5d0e0f789e442e4aea1bd82d
Daniel Kendell <daniel.kendell@gmail.com>
2017-01-22 21:36:58 +0000

    added hlerr

--- aliases/general.sh
+++ aliases/general.sh
@@ -20,3 +20,8 @@ function cdd {
 function hl {
     sed -e "s/\($1\)/\x1b[7m\1\x1b[0m/g"
 }
+
+# http://stackoverflow.com/questions/1763891/can-stdout-and-stderr-use-different-colors-under-xterm-konsole
+hlerr()(
+  set -o pipefail;"$@" 2>&1>&3 | sed $'s,.*,\e[91m\e[1m&\e[7m,' >&2
+) 3>&1
