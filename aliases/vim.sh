6d5385357b3f851d4e8ce123fbb48b28052a4564
Daniel Kendell <daniel.kendell@gmail.com>
2017-08-22 17:19:16 +0100

    bin/'d ew

--- aliases/vim.sh
+++ aliases/vim.sh
@@ -4,13 +4,3 @@ alias se="sudo vim"
 alias ep="e ${DEV_DIR}/mduk/dotfiles/ps1.sh"
 alias evrc="e ~/.vimrc"
 alias ehosts="se /etc/hosts"
-
-ew() {
-    path="$(which $1)"
-    owner="$(ls -ld $path | awk '{print $3}')"
-    cmd="$EDITOR"
-    if [[ "$owner" != "$USER" ]]; then
-        cmd="sudo $EDITOR"
-    fi
-    $cmd "$path"
-}
