9792b5040c107330155352530dc3a8db28af2c1d
Daniel Kendell <daniel.kendell@gmail.com>
2017-09-05 17:39:16 +0100

    bin/'d ytdla and ytdlv

--- aliases/youtube.sh
+++ aliases/youtube.sh
@@ -1,14 +1,5 @@
 alias ytdll="youtube-dl -F"
 
-function ytdla() {
-    pushd ~/Music
-    youtube-dl \
-        -f m4a \
-        --output "%(uploader)s/%(title)s.%(ext)s" \
-        "${1:-$(pbpaste)}"
-    popd
-}
-
 function ytdlapl() {
     pushd ~/Music
     youtube-dl \
@@ -20,15 +11,6 @@ function ytdlapl() {
 
 
 
-function ytdlv() {
-    pushd ~/Videos
-    youtube-dl \
-        -f best \
-        --output "%(uploader)s/%(release_date)s - %(title)s.%(ext)s" \
-        --write-info-json \
-        "${1:-$(pbpaste)}"
-    popd
-}
 
 function ytdlvpl() {
     pushd ~/Videos
