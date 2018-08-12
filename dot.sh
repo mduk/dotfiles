a379990c457ab8a827c71eab3300512b95bf9871
Daniel Kendell <daniel@kendell.org.uk>
2018-08-12 23:29:23 +0100

    explicitly dot dir

--- dot.sh
+++ dot.sh
@@ -16,13 +16,13 @@ source "$DOT_DIR/path.sh"
 source "$DOT_DIR/ps1.sh"
 #source "$DOT_DIR/powerline.sh"
 
-for file in $(find ./commands -name '*.alias')
+for file in $(find $DOT_DIR/commands -name '*.alias')
 do
   declare alias=$(basename $file | sed -r 's/(.*).alias/\1/')
   declare command=$(cat $file)
   alias "${alias}=${command}"
 done
 
-for function in $(find ./commands -name '*.function')
+for function in $(find $DOT_DIR/commands -name '*.function')
 do source "$function"
 done
