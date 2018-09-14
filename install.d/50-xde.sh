e71a269391de89c99f8421457670e682ec57a769
Daniel Kendell <daniel@kendell.org.uk>
2018-09-14 11:06:41 +0100

    rearranged lines. apparently dmenu isn't a package, it's in suckless-tools, added read-edid

--- install.d/50-xde.sh
+++ install.d/50-xde.sh
@@ -1,17 +1,22 @@
 dot-install package xsel
 dot-install package arandr
+dot-install package read-edid
+
 dot-install package xscreensaver
 dot-install package xscreensaver-data-extra
 dot-install package xscreensaver-gl
 dot-install package xscreensaver-gl-extra
 dot-install package xscreensaver-screensaver-bsod
-dot-install package feh
-dot-install package dmenu
+
 dot-install package i3-wm
 dot-install package i3-wm-dbg
+dot-install package feh
+dot-install package suckless-tools
+
+dot-install package dconf-editor
 dot-install package konsole
+
 dot-install package evince
 dot-install package gimp
 dot-install package libreoffice-writer
 dot-install package libreoffice-calc
-dot-install package dconf-editor
