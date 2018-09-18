e3a7b0422e105c10b5e826a8f80e82f30461c42d
Daniel Kendell <daniel@kendell.org.uk>
2018-09-18 14:23:55 +0100

    monitor arrangement adjustment

--- home/daniel/.screenlayout/openbanking.sh
+++ home/daniel/.screenlayout/openbanking.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x352 --rotate normal --output DP-2 --off
+xrandr --output DP-3 --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x720 --rotate normal --output DP-2 --off
