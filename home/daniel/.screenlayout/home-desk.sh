5066c17934d932291674f879fbc11eb0fcf21892
Daniel Kendell <daniel@kendell.org.uk>
2019-01-12 00:19:41 +0000

    screenlayouts

--- home/daniel/.screenlayout/home-desk.sh
+++ home/daniel/.screenlayout/home-desk.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
+xrandr --output DP-3 --mode 1920x1080 --pos 288x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x1080 --rotate normal --output DP-2 --off
--- home/daniel/.screenlayout/screencast.sh
+++ home/daniel/.screenlayout/screencast.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 912x2160 --rotate normal --output DP-2 --off
+xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal \
+       --output DP-1 --off \
+       --output eDP-1 --primary --mode 1920x1080 --pos 912x2160 --rotate normal \
+       --output DP-2 --off
