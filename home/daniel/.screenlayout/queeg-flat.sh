94f1db136bc2fddc06c7cf25439b8d6e9e3604e9
Daniel Kendell <daniel.kendell@gmail.com>
2019-04-10 21:35:58 +0100

    auto-monitors works properly on queeg

--- home/daniel/.screenlayout/queeg-flat.sh
+++ home/daniel/.screenlayout/queeg-flat.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output VGA-1 --off --output LVDS-1 --mode 1280x720 --pos 304x1080 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr \
+  --output VGA-1 --off \
+  --output LVDS-1 --mode 1280x720 --pos 0x2160 --rotate normal \
+  --output HDMI-3 --off \
+  --output HDMI-2 --off \
+  --output HDMI-1 --off \
+  --output DP-3 --off \
+  --output DP-2 --off \
+  --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal
