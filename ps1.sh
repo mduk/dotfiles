5c0b9746ae68534e17481c140535d89d5c86bd18
Daniel Kendell <daniel.kendell@gmail.com>
2019-11-07 10:41:50 +0000

    formatting

--- ps1.sh
+++ ps1.sh
@@ -30,21 +30,20 @@ unprompt() {
 }
 
 terminal_width() {
-    local cols=$(tput cols)
+  local cols=$(tput cols)
 
-    if [[ $cols -eq 210 ]]; then
-        echo "1"
-    elif [[ $cols -lt 210 ]] && [[ $cols -ge 103 ]]; then
-        echo "2"
-    elif [[ $cols -lt 103 ]] && [[ $cols -ge 68 ]]; then
-        echo "3"
-    else
-        echo "4"
-    fi
+  if [[ $cols -eq 210 ]]
+  then echo "1"
+  elif [[ $cols -lt 210 ]] && [[ $cols -ge 103 ]]
+  then echo "2"
+  elif [[ $cols -lt 103 ]] && [[ $cols -ge 68 ]]
+  then echo "3"
+  else echo "4"
+  fi
 }
 
 exit_bar() {
-    echo "\033[48;5;\$([[ \$? -gt 0 ]] && echo \"88\" || echo \"22\")m\033[K\033[0m"
+  echo "\033[48;5;\$([[ \$? -gt 0 ]] && echo \"88\" || echo \"22\")m\033[K\033[0m"
 }
 
 block_wifi() {
@@ -103,25 +102,25 @@ block_path() {
 }
 
 block_screen() {
-    if [[ -n $STY ]]
-    then echo "$PROMPT_YELLOW[$STY]"
-    fi
+  if [[ -n $STY ]]
+  then echo "$PROMPT_YELLOW[$STY]"
+  fi
 }
 
 block_host() {
-    if [[ ! -z "$SSH_TTY" ]]; then
-      echo "$PROMPT_YELLOW[$USER@$(hostname)]"
-    fi
+  if [[ ! -z "$SSH_TTY" ]]
+  then echo "$PROMPT_YELLOW[$USER@$(hostname)]"
+  fi
 }
 
 block_user() {
-    local user=$(whoami)
+  local user=$(whoami)
 
-    if [[ "$user" == "root" ]]
-    then echo "$PROMPT_BOLD$PROMPT_RED[ROOT]$PROMPT_RESET"
-    elif [[ "$user" != "$MY_USERNAME" ]]
-    then echo "[\u]"
-    fi
+  if [[ "$user" == "root" ]]
+  then echo "$PROMPT_BOLD$PROMPT_RED[ROOT]$PROMPT_RESET"
+  elif [[ "$user" != "$MY_USERNAME" ]]
+  then echo "[\u]"
+  fi
 }
 
 block_git() {
@@ -142,63 +141,65 @@ block_git() {
 # THE PROMPT
 ################################################################################
 prompt_command() {
-    CMD_EXIT=$?
-    local term_lines=$(tput lines)
-    local term_cols=$(tput cols)
+  CMD_EXIT=$?
+  local term_lines=$(tput lines)
+  local term_cols=$(tput cols)
 
-    PS1=""
+  PS1=""
 
-    # Exit Bar
-    if [[ $PROMPT_BAR == true ]]; then
-        PS1="${PS1}$(exit_bar)"
-    fi
+  # Exit Bar
+  if [[ $PROMPT_BAR == true ]]
+  then PS1="${PS1}$(exit_bar)"
+  fi
 
-    # Command Space
-    for i in $(seq ${PROMPT_SPACE:-$DEFAULT_PROMPT_SPACE}); do
-        PS1="$PS1\n"
-    done
+  # Command Space
+  for i in $(seq ${PROMPT_SPACE:-$DEFAULT_PROMPT_SPACE})
+  do PS1="$PS1\n"
+  done
 
-    # Environment Line
-    PS1="${PS1}$(block_screen)"
-    PS1="${PS1}$(block_user)"
-    PS1="${PS1}$(block_host)"
+  # Environment Line
+  PS1="${PS1}$(block_screen)"
+  PS1="${PS1}$(block_user)"
+  PS1="${PS1}$(block_host)"
 
-    # PATH
-    if [[ $PROMPT_PATH == true ]] \
-    && [[ "$(pwd)" != "$HOME" ]]
-    then PS1="${PS1}$(block_path)"
-    fi
+  # PATH
+  if [[ $PROMPT_PATH == true ]] \
+  && [[ "$(pwd)" != "$HOME" ]]
+  then PS1="${PS1}$(block_path)"
+  fi
 
-    # Git
-    if [[ $PROMPT_GIT == true ]] \
-    && git branch &>/dev/null
-    then
-      PS1="${PS1}$(block_git)"
-    fi
+  # Git
+  if [[ $PROMPT_GIT == true ]] \
+  && git branch &>/dev/null
+  then
+    PS1="${PS1}$(block_git)"
+  fi
 
 
-    # Versions Line
-    if [[ $PROMPT_VERSIONS != false ]]; then
-        PS1="$PS1\n"
-        PS1="${PS1}$(block_php)"
-        PS1="${PS1}$(block_ruby)"
-        PS1="${PS1}$(block_python)"
-    fi
+  # Versions Line
+  if [[ $PROMPT_VERSIONS != false ]]
+  then
+    PS1="$PS1\n"
+    PS1="${PS1}$(block_php)"
+    PS1="${PS1}$(block_ruby)"
+    PS1="${PS1}$(block_python)"
+  fi
 
-    # Prompt Line
-    if [[ $PROMPT_CLOCK == true ]]; then
-        PS1="$PS1\n"
-        PS1="${PS1}$(block_clock)"
-    fi
+  # Prompt Line
+  if [[ $PROMPT_CLOCK == true ]]
+  then
+    PS1="$PS1\n"
+    PS1="${PS1}$(block_clock)"
+  fi
 
-    # Insert Custom Directory Prompt
-    if [[ -f "$(pwd)/.ps1" ]]; then
-        PS1="${PS1}$(source "$(pwd)/.ps1")\n"
-    fi
+  # Insert Custom Directory Prompt
+  if [[ -f "$(pwd)/.ps1" ]]
+  then PS1="${PS1}$(source "$(pwd)/.ps1")\n"
+  fi
 
-    PS1="${PS1}$PROMPT_BOLD\$$PROMPT_RESET "
+  PS1="${PS1}$PROMPT_BOLD\$$PROMPT_RESET "
 
-    export PS1
+  export PS1
 }
 
 PROMPT_COMMAND=prompt_command
