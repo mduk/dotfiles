4c3a7728b0fbce3b361799feeba8f9cd60ebf467
Daniel Kendell <daniel.kendell@gmail.com>
2020-08-10 09:09:21 +0100

    i3: trackpad and touchscreen toggles

--- home/daniel/.screenlayout/eddie-flat.sh
+++ home/daniel/.screenlayout/eddie-flat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 672x2160 --rotate normal --output DP-2 --off
+xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 3840x720 --rotate normal --output DP-2 --off
