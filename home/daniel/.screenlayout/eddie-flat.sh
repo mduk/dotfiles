db952c704b234c25ce9138b3acced008644df062
Daniel Kendell <daniel.kendell@gmail.com>
2021-08-24 09:21:07 +0100

    auto-monitors

--- home/daniel/.screenlayout/eddie-flat.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 3840x720 --rotate normal --output DP-2 --off
