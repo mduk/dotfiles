9a7895e0c0ea8e74700974af26df991aa450523e
Daniel Kendell <daniel.kendell@gmail.com>
2022-07-25 11:06:01 +0100

    holly monitor trackpad shiz

--- home/daniel/.screenlayout/eddie-desk.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate right --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 2160x1152 --rotate normal --output DP-2 --off
