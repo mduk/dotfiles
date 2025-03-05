59a76320748818df439c38fe208ac52e93da5b8c
Daniel Kendell <daniel.kendell@gmail.com>
2025-03-05 17:40:49 +0000

    install_docker and install_firefox

---
+++ install_docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Signing key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update & Install
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Post-install
+
+# sudo groupadd docker
+
+echo "Adding $USER to docker group"
+sudo usermod -aG docker $USER
