73559eb404d6307ccec3d60852e5795fa8a655cd
Daniel Kendell <daniel.kendell@gmail.com>
2025-03-11 22:43:28 +0000

    moving stuff about

---
+++ install_git.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install -y git
+git config --global user.email "daniel.kendell@gmail.com"
+git config --global user.name "Daniel Kendell"
--- installcmds.sh
+++ installcmds.sh
@@ -3,44 +3,27 @@ sudo apt update -y
 sudo apt install -y adwaita-icon-theme-full adwaita-qt tcl-awthemes lxappearance
 sudo apt install -y bc gnuplot
 sudo apt install -y binutils build-essential pkg-config cmake
-
-# Install using docker's own APT repository
-# sudo apt install -y docker.io docker-compose kubernetes
-
 sudo apt install -y dos2unix
 sudo apt install -y exfatprogs
-
-# Install using mozilla's own APT repository_
-#sudo apt install -y firefox chromium-browser
-
-sudo apt install -y i3-wm suckless-tools feh polybar scrot dunst keychain
-sudo apt install -y inkscape gimp
-sudo apt install -y mpd mpc ncmpc kid3 flac mediainfo
-sudo apt install -y mpv vlc ffmpeg dvdbackup handbrake sox
-sudo apt install -y v4l-utils qv4l2
-sudo apt install -y obs-studio
-sudo apt install -y nginx nginx-doc
-sudo apt install -y npm
-sudo apt install -y openscad
-sudo apt install -y openssh-server openssh-client
-sudo apt install -y pavucontrol pavumeter pamix
-sudo apt install -y pipenv
-sudo apt install -y rxvt-unicode
-sudo apt install -y telnet nmap openssl whois
-sudo apt install -y tmux vim git jq httpie
-sudo apt install -y tree
-sudo apt install -y virtualbox vagrant
-sudo apt install -y xscreensaver xscreensaver-data-extra
-sudo apt install -y xsel arandr read-edid xclip x11-utils
-sudo apt install -y kicad
-sudo apt install -y xvkbd xbindkeys libinput-tools
-sudo apt install -y transmission-gtk
 sudo apt install -y p7zip-full
 sudo apt install -y ncat
+sudo apt install -y tree
+sudo apt install -y openssh-server openssh-client
+sudo apt install -y mpd mpc ncmpc kid3 flac mediainfo
+sudo apt install -y mpv vlc ffmpeg dvdbackup handbrake sox
+sudo apt install -y telnet nmap openssl whois
+sudo apt install -y tmux vim git jq httpie
+sudo apt install -y i3-wm suckless-tools feh polybar scrot dunst keychain
+sudo apt install -y xsel arandr read-edid xclip x11-utils
+sudo apt install -y v4l-utils qv4l2
+sudo apt install -y pavucontrol pavumeter pamix
+sudo apt install -y rxvt-unicode
+sudo apt install -y inkscape gimp
+sudo apt install -y xscreensaver xscreensaver-data-extra
+sudo apt install -y xvkbd xbindkeys libinput-tools
 
 sudo apt autoremove -y
 
-exit
 
 sudo sed -i \
   -e '/^GRUB_TIMEOUT=/s/=.*/=3/' \
@@ -48,6 +31,8 @@ sudo sed -i \
   -e '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/=.*/=""/' \
   /etc/default/grub
 sudo update-grub
+
+exit
 
 pulse_tcp_listener=/etc/pulse/default.pa.d/tcp-listener.pa
 if [[ ! -f $pulse_tcp_listener ]]
@@ -58,8 +43,5 @@ fi
 # missing: install mpd.conf
 
 sudo adduser $USER dialout
-
-git config --global user.email "daniel.kendell@gmail.com"
-git config --global user.name "Daniel Kendell"
 
 xrdb ~/.Xresources
