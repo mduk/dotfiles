61744e4bc26ca37a50e3066b3dab316defdd1ce7
Daniel Kendell <daniel.kendell@gmail.com>
2025-03-31 22:49:59 +0100

    install_signal.sh tidy

--- install_signal.sh
+++ install_signal.sh
@@ -3,6 +3,7 @@
 # 1. Install our official public software signing key:
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+rm signal-desktop-keyring.gpg
 
 # 2. Add our repository to your list of repositories:
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
